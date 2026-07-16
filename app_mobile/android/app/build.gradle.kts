1dc4acdf9c7de607ab5510771aac9f0b6bd3cc00
Djohnni <djohnni@gmail.com>
2026-07-16 16:23:24 -0300

    chore: correct android release version to 18

--- app_mobile/android/app/build.gradle.kts
+++ app_mobile/android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.ia4tube.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.2.7"
+        versionCode = 18
+        versionName = "0.2.9"
 
         buildConfigField("String", "API_BASE", "\"https://ia4tube-api.onrender.com\"")
     }
