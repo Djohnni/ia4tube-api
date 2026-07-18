ae1e8b0a175b8a8174c498850d94bd5f8adba0c8
Djohnni <djohnni@gmail.com>
2026-07-18 14:24:13 -0300

    chore: bump android version to 0.2.11

--- app_mobile/android/app/build.gradle.kts
+++ app_mobile/android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.ia4tube.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "0.2.10"
+        versionCode = 22
+        versionName = "0.2.11"
 
         buildConfigField("String", "API_BASE", "\"https://ia4tube-api.onrender.com\"")
     }
